12a7f63c95e5cc1b037bebcf4f29907b3fc4b590
Mehmet Yıldız <yildiz@cisco.com>
2023-12-17 16:23:44 +0300

    refactoring

--- src/test/java/com/webex/events/com/webex/events/error/ErrorResponseTest.java
+++ src/test/java/com/webex/events/com/webex/events/error/ErrorResponseTest.java
@@ -45,4 +45,60 @@ public class ErrorResponseTest {
         Extensions extensions = errResponse.extensions;
         assertEquals(errorList.get("first_name"), extensions.getErrors().get("first_name"));
     }
+
+    @Test
+    void testServerError() throws JsonProcessingException {
+        HashMap<String, Object> data = new HashMap<>() {
+            {
+                put("message", "Server Error");
+                put("extensions", new HashMap<String, Object>(){{
+                    put("code", "INTERNAL_SERVER_ERROR");
+                    put("referenceId", "somereferenceid");
+                }});
+            }
+        };
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(data);
+
+        ObjectMapper mapper = new ObjectMapper();
+        ErrorResponse errResponse;
+        errResponse = mapper.readValue(json, ErrorResponse.class);
+
+        assertEquals("Server Error", errResponse.getMessage());
+        assertEquals("INTERNAL_SERVER_ERROR", errResponse.getCode());
+        assertEquals("somereferenceid", errResponse.extensions.getReferenceId());
+    }
+
+    @Test
+    void testRateLimitingDetails() throws JsonProcessingException {
+        HashMap<String, Object> data = new HashMap<>() {
+            {
+                put("message", "Max cost exceed.");
+                put("extensions", new HashMap<String, Object>(){{
+                    put("code", "MAX_COST_EXCEEDED");
+                    put("cost", 45);
+                    put("availableCost", 5);
+                    put("threshold", 50);
+                    put("dailyThreshold", 200);
+                    put("dailyAvailableCost", 190);
+                }});
+            }
+        };
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(data);
+
+        ObjectMapper mapper = new ObjectMapper();
+        ErrorResponse errResponse;
+        errResponse = mapper.readValue(json, ErrorResponse.class);
+
+        assertEquals("Max cost exceed.", errResponse.getMessage());
+        assertEquals("MAX_COST_EXCEEDED", errResponse.getCode());
+        assertEquals(45, errResponse.extensions.getCost());
+        assertEquals(5, errResponse.extensions.getAvailableCost());
+        assertEquals(50, errResponse.extensions.getThreshold());
+        assertEquals(200, errResponse.extensions.getDailyThreshold());
+        assertEquals(190, errResponse.extensions.getDailyAvailableCost());
+    }
 }
